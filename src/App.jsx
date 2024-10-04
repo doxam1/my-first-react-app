a168e7a222c5b5e198262eace6ed8626bfa68093
doxam1 <143098976+doxam1@users.noreply.github.com>
2024-10-04 22:24:08 +0300

    style for hello world with className

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <h1> hello world ! </h1>
+      <h1 className='helloWorld'> hello world ! </h1>
     </>
   )
 }
